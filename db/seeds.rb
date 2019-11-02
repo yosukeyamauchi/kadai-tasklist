824b6166dd77ab97c9f38755e4905456a905065d
yosuke.yamauchi <yosuke.yamauchi0129@gmail.com>
2019-11-02 09:28:40 +0000

    update db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
-(1..100).each do |number|
-  Task.create(content: 'てすとcontent' + number.to_s, status: 'てすとstatus')
+(1..10).each do |number|
+  User.create(
+    name: 'てすとゆーざー' + number.to_s,
+    email: 'test_user' + number.to_s + '@test.com',
+    password_digest: 'test'
+  )
+end
+
+(1..10).each do |user_number|
+  user_id = user_number
+
+  (1..10).each do |task_number|
+    Task.create(
+      content: 'てすとたすく' + task_number.to_s,
+      status: 'かんりょう',
+      user_id: user_id
+    )
+  end
 end
